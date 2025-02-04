516de53560d7a5bc0148967a534c94b2c8a42732
Naman Bansal <namanbansal102@gmail.com>
2025-02-05 02:34:30 +0530

    Feature Of  Upvoting project

--- main.sol
+++ main.sol
@@ -2,6 +2,10 @@
 pragma solidity >=0.7.0;
 pragma experimental ABIEncoderV2;
 contract compete {
+    struct pair{
+        uint project_id;
+        uint upvotes;
+    }
    struct build {
     uint project_id;
     uint upvotes;
@@ -13,8 +17,10 @@ contract compete {
     address[] team;// first is the owner of the team
    }
    mapping (address => uint[]) userbuilds_map;
+   event deposit(address sender,uint amount);
    struct hacakthon {
     uint hack_id;
+    address hack_owner;
     string title;
     string img_url;
     string desc;
@@ -22,7 +28,9 @@ contract compete {
     string st_date;
     string end_date;
     string mode; 
+    uint funding;
    }
+    mapping (uint =>pair[]) hack_upvotes_map;
    uint hack_id=0;
    uint project_id=0;
    hacakthon[] public hackathons_arr;// we have created a hacakthon array
@@ -30,7 +38,7 @@ contract compete {
     build[] public build_arr;
 
     function registerHackathon(string memory _title,string memory _img_url,string memory _desc,uint  _prizePool,string memory _st_date,string memory _end_date,string memory _mode)  public {
-        hackathons_arr.push(hacakthon(hack_id,_title,_img_url,_desc,_prizePool,_st_date,_end_date,_mode));
+        hackathons_arr.push(hacakthon(hack_id,msg.sender,_title,_img_url,_desc,_prizePool,_st_date,_end_date,_mode,0));
         hack_id++;
     }
 
@@ -56,6 +64,7 @@ contract compete {
     function submitbuild (uint _hack_id,uint _project_id) public {
         // for submitting build in my hacakthon project
         hack_map[_hack_id].push(_project_id);
+        hack_upvotes_map[_hack_id].push(pair(_project_id,0));
     }
 
     function getallbuilds() public view returns(build[] memory ) {
@@ -76,6 +85,21 @@ contract compete {
         }
         return my_builds;
     }
-    public view 
-    // we also have to store the login timehistory
+    function upvoteProject(uint _hack_id,uint _project_id) external payable {
+        pair[] memory pairs_array=hack_upvotes_map[_hack_id];
+        for (uint256 i = 0; i < pairs_array.length; i++) {
+            if (pairs_array[i].project_id==_project_id) {
+            require(msg.value>=2,"Must Send Ether 1");
+                pairs_array[i].upvotes++;
+                emit deposit(msg.sender, msg.value);
+                break;
+            }
+        }
+        build memory my_build=build_arr[_project_id];
+        my_build.upvotes++;
+    }
+    function end_hackathon(uint _hack_id)external payable{
+        // now the winner is announced
+        require(hackathons_arr[_hack_id].hack_owner==msg.sender,"Not The Owner Of hacakthon");
+    }
 }
