f5564e03e97e2e26eac3d3308a545b0be62b58f8
Naman Bansal <namanbansal102@gmail.com>
2025-02-05 15:41:19 +0530

    Initialize Next app

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+// import {Button} from "@heroui/react";
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@heroui/react";
+import {Badge, Avatar} from "@heroui/react";
+
+export default function Home() {
+  return (
+   <>
+   <Button color="primary">dfdfq</Button>
+   <Badge color="primary" content="5">
+      <Avatar radius="md" size="lg" src="https://i.pravatar.cc/300?u=a042581f4e29026709d" />
+    </Badge>
+
+   </>
+  );
+}
