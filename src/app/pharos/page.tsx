949d3eed379613f4e220d0034d513589626de47b
Naman Bansal <namanbansal102@gmail.com>
2025-05-11 01:35:05 +0530

    Deployed

--- src/app/pharos/page.tsx
+++ src/app/pharos/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-2xl font-bold mb-4">SEI Network Video</h1>
+      <h1 className="text-2xl font-bold mb-4">Pharos Network Video</h1>
       <video
         controls
         className="w-full max-w-3xl rounded-lg shadow-lg"
